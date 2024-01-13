87f6d0f71e128e96b635317cff67d58a2a1a35e3
webler-biology-physiology <csabavadasz79@gmail.com>
2024-01-13 14:12:01 +0100

    create WebController using thymeleaf, index.html

--- src/main/java/hu/webler/weblerspringbasicthymeleaf/controller/StudentController.java
+++ src/main/java/hu/webler/weblerspringbasicthymeleaf/controller/StudentController.java
@@ -3,6 +3,7 @@ package hu.webler.weblerspringbasicthymeleaf.controller;
 import hu.webler.weblerspringbasicthymeleaf.bootstrap.DataInitializer;
 import hu.webler.weblerspringbasicthymeleaf.model.Student;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,14 @@ public class StudentController {
         this.dataInitializer = dataInitializer;
     }*/
 
-    @GetMapping("/students")
+    /*@GetMapping(value = "/students",
+        produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Student> getStudents() {
+        // Visszaadjuk a diákok listáját REST API-n keresztül
+        return dataInitializer.getStudents();
+    }*/
+
+    @GetMapping(value = "/students")
     public List<Student> getStudents() {
         // Visszaadjuk a diákok listáját REST API-n keresztül
         return dataInitializer.getStudents();
